2719b08ba295c08b634197140f814541fc01236f
Borisova-EV <72605569+Borisova-EV@users.noreply.github.com>
2021-03-16 10:13:09 +0300

    добавила цикл для addbookmark

--- js/addbookmark.js
+++ js/addbookmark.js
@@ -1,10 +1,12 @@
-const bookmarkButtons = document.querySelector(".button-bookmark");
+const bookmarkButtons = document.querySelectorAll(".button-bookmark");
 const bookmark = document.querySelector(".bookmark-link");
 const bookmarkCounter = document.querySelector(".bookmark-counter");
 let counter = "";
 
-bookmarkButtons.addEventListener("click", function () {
+for (let bookmarkButton of bookmarkButtons) {
+  bookmarkButton.addEventListener("click", function () {
   bookmark.classList.add("full");
   counter++;
   bookmarkCounter.textContent = counter;
 })
+}
